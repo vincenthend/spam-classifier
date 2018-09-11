5dc0772c78b721ebb7ef2e1f09267f947711ea60
Luthfi Fadillah <luthfi.fadillah@outlook.com>
2018-09-11 16:05:27 +0700

    implement mailClassifier, not tested

--- MailClassifier.py
+++ MailClassifier.py
@@ -1,9 +1,26 @@
+from MailData import MailData,MailDataLoader
+import MailProcessor
+import HTMLStripper
+
 class classifier:
     def __init__(self):
         # Load trained model
-        pass
+
+        # Load Testing mailData
+        self.mailDataTesting = <ailDataLoader.loadTesting()
     
-    def classify(self, mailData):
+    def classify(self, model, mailDataTesting):
+        # Preprocess mailDataTesting
+        preprocessor = MailProcessor.Preprocessor();
+
+        payload = []
+
+        for mailData in mailDataTesting:
+            text = HTMLStripper.stripTags(mailData.payload)
+            text = HTMLStripper.cleanText(text)
+            payload.append(text)
+
+        
         pass 
 
     
